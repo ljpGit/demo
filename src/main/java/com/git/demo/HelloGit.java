93c02c70e2d890f59af8198d406fbc40e36b40e6
ljpGit <ljp_wy@163.com>
2016-12-25 16:27:13 +0800

    four

--- src/main/java/com/git/demo/HelloGit.java
+++ src/main/java/com/git/demo/HelloGit.java
@@ -6,6 +6,6 @@ public class HelloGit {
 		System.out.println("three git");
 		System.out.println("second git");
 		System.out.println("five git");
-		System.out.println("six git");
+		System.out.println("four git");
 	}
 }

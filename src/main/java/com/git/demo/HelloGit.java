482f2230e9f528f4473140936f5884d41ece1c70
ljpGit <ljp_wy@163.com>
2016-12-25 15:28:15 +0800

    了解了解了解

--- src/main/java/com/git/demo/HelloGit.java
+++ src/main/java/com/git/demo/HelloGit.java
@@ -3,6 +3,6 @@ package com.git.demo;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("first git");
-		System.out.println("first git");
+		System.out.println("second git");
 	}
 }

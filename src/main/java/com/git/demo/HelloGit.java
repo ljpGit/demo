d74c0e530af59a95cda7ceb022b88bd8ae4c343a
ljpGit <ljp_wy@163.com>
2017-01-05 22:48:37 +0800

    dlkjlskdj

--- src/main/java/com/git/demo/HelloGit.java
+++ src/main/java/com/git/demo/HelloGit.java
@@ -7,10 +7,6 @@ public class HelloGit {
 		System.out.println("second git");
 		System.out.println("five git");
 		System.out.println("four git");
-<<<<<<< HEAD
 		int a;
-=======
-		int ab;
->>>>>>> branch 'master' of https://github.com/ljpGit/demo.git
 	}
 }

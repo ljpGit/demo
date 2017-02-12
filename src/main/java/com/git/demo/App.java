115fbf7c483e555c79448a44046e0b6b2bebc6f9
ljpGit <ljp_wy@163.com>
2017-02-12 20:45:15 +0800

    dfgfdg

--- src/main/java/com/git/demo/App.java
+++ src/main/java/com/git/demo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "hl World!" );
+        System.out.println( "hel World!" );
     }
 }

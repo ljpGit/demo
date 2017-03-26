fea35841ee7f0ec2924f9ce86d8f3783fd7d9913
ljpGit <ljp_wy@163.com>
2017-03-26 22:27:31 +0800

    i

--- src/main/java/com/git/demo/App.java
+++ src/main/java/com/git/demo/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int j;
+    	int i;
         System.out.println( "Hello World!" );
         System.out.println( "hello World!" );
 

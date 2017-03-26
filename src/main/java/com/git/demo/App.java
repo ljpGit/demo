5eaa3035bbedbb7ff2476b5a30ebaa2b9651156a
ljpGit <ljp_wy@163.com>
2017-03-26 22:43:12 +0800

    hWorld

--- src/main/java/com/git/demo/App.java
+++ src/main/java/com/git/demo/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	int i;
-        System.out.println( "Hello World!" );
+        System.out.println( "HWorld!" );
         System.out.println( "hello World!" );
         System.out.println("gitBash");
 

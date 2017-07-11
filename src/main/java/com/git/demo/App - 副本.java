05f18bd1caa7724b512db5a5edea9515af2dbd9f
ljpGit <1004632650@qq.com>
2017-07-11 13:45:07 +0800

    123

---
+++ src/main/java/com/git/demo/App - 副本.java
@@ -0,0 +1,19 @@
+package com.git.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int i;
+        System.out.println( "HWorld" );
+        System.out.println( "HW!" );
+        System.out.println( 111 );
+        System.out.println("gitBash");
+		System.out.println("gitBash");
+
+    }
+}

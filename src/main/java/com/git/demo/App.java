e113952510ee82d3cf504483de1dc7cc8ef80025
liujinpei <jinpei.liu@frontpay.cn>
2017-07-11 10:45:04 +0800

    copy

--- src/main/java/com/git/demo/App.java
+++ src/main/java/com/git/demo/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println( "HWorld" );
         System.out.println( "HW!" );
         System.out.println( "hello World!" );
+        System.out.println( "hello World!" );
         System.out.println("gitBash");
 
 
